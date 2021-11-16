ee2e92548ae33865f5c9c085531fd8ee06852430
Stackbit Code Editor <projects@stackbit.com>
2021-11-16 16:00:24 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 sourcebit.fetch(sourcebitConfig);
 
+test
+
 module.exports = withBundleAnalyzer({
     trailingSlash: true,
     devIndicators: {
